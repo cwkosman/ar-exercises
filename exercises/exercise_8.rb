3f2da2777afe4ccec4cd691df43751918c536951
cwkosman <cwkosman@gmail.com>
2017-03-14 20:00:45 +0000

    Exercise 8 (stretch) complete

---
+++ exercises/exercise_8.rb
@@ -0,0 +1,14 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
+require_relative './exercise_7'
+
+puts "Exercise 8"
+puts "----------"
+
+@chuck = @store2.employees.create(first_name: 'Chuck', last_name: 'Kosman', hourly_rate: 50)
+puts @chuck.password
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,19 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  validates_presence_of :first_name, :last_name, :store
+  validates :hourly_rate, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 40,
+    less_than_or_equal_to: 200
+  }
+
+  before_create :generate_password
+
+  private
+
+  def generate_password
+    return unless password.nil?
+    self.password = [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+  end
 end
